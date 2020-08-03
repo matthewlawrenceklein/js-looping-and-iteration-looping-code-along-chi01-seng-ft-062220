9b4524b5c8dd1bfbf13ef6fab76ccfa5d16ae939
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-08-03 12:38:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+
+    let cards = []
+    for (let i = 0; i < array.length; i++){
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(num){
+    let countdown = num
+    while (countdown >= 0) {
+        console.log(countdown)
+        countdown-=1
+    }
+}
+
